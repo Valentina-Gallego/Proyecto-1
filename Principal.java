2b63a3b7801a7bc5e4077b94000be0db7e4fe30f
Sala K 405 <salak405@salasfr.itm.edu.co>
2026-02-16 09:07:10 -0500

    cambio nombre Scanner

--- Principal.java
+++ Principal.java
@@ -7,7 +7,7 @@ public class Principal {
         Metodos m = new Metodos();
         int cant = 0;
         System.out.println("Ingrese la dimension del patio ");
-        cant = teclado.nextInt();
+        cant = sc.nextInt();
         objVehiculo[] r = new objVehiculo[cant];//.
         r = m.LlenarRegistros(r);
         r = m.CalcularNuevoPago(r);
